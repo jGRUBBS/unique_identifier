f5658b6ee0aab866df65cb2940967661412f9724
John Royall <johnroyall7@yahoo.com>
2018-03-19 00:30:25 -0400

    refactor spec helper `build_dummy_class` method to extract out base class into `build_dummy_base_class`
    - this way a base class can be created and tested without `unique_id`
    having been called on it already

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,19 +29,11 @@ RSpec.configure do |config|
 end
 
 
-def build_dummy_class(unique_attr_name, unique_options = {}, class_options = {})
-  # setup class and include delayed_cron
-
-  class_name = "DummyModel"
-
-  ActiveRecord::Base.send(:include, UniqueIdentifier::Glue)
-  Object.send(:remove_const, class_name) rescue nil
-
-  # Set class as a constant
-  klass = Object.const_set(class_name, Class.new(ActiveRecord::Base))
+def build_dummy_class(unique_attr_name = nil, unique_options = {}, class_options = {})
+  # setup class and include unique_identifier
+  klass = build_dummy_base_class
 
   klass.class_eval do
-
     unique_id unique_attr_name, unique_options
 
     if class_options.fetch(:validate, false)
@@ -49,5 +41,16 @@ def build_dummy_class(unique_attr_name, unique_options = {}, class_options = {})
     end
   end
 
+  klass
+end
+
+def build_dummy_base_class
+  class_name = "DummyModel"
+
+  ActiveRecord::Base.send(:include, UniqueIdentifier::Glue)
+  Object.send(:remove_const, class_name) rescue nil
+
+  # Set class as a constant
+  klass = Object.const_set(class_name, Class.new(ActiveRecord::Base))
   klass
 end
